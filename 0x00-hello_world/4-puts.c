d2fe735a740dee6445f8c45070ad9492c4f547fa
Aya Ragab <git@github.com>
2023-03-16 11:01:30 -0700

    hgd

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-/*
+/**
  * main - A program that prints a sentence using puts()
- * Returns: 0 (Success)
+ * Return: 0 (Success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
