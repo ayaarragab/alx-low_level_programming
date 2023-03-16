89d5a766683809021d056f8a59dd9252c29ed020
Aya Ragab <git@github.com>
2023-03-16 11:30:50 -0700

    b

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 /**
- * main - prints with proper grammar, 
- * but the outcome is a piece of art,
- * Return: 0 if exited properly, non-zero otherwise
+ * main	-A program
+ * Return:0
  */
 int main(void)
 {
