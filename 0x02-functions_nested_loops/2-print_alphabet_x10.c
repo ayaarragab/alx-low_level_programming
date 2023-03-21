48e4515723371259844a810b3d5b70a85b81d884
Aya Ragab <git@github.com>
2023-03-21 14:05:33 -0700

    dsf

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,17 @@
 #include "main.h"
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet
+ * Return: nothing
+ */
 void print_alphabet_x10(void)
 {
+	char count;
+	char new_line = '\n';
 	int i = 0;
-	char count, new_line = '\n';
 
-	while (i <= 9 )
+	while (i <= 9)
 	{
-		for (count = 'a'; count <= 'z' ; count++ )
+		for (count = 'a'; count <= 'z' ; count++)
 		{
 			_putchar(count);
 		}
