675cae360291f542224478c6c00a8e47b8dabdf3
Aya Ragab <git@github.com>
2023-03-21 16:44:47 -0700

    ad

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*print_to_98 - a function that prints
+*@n: variable test
+*Return: nothing
+*/
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n ; i <= 9ii8 ; i++)
+	{
+		_putchar(i);
+	}
+}
