492d371a1906714263984a6a50b0d53eb66684eb
Aya Ragab <git@github.com>
2023-03-21 12:48:40 -0700

    dsd

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Descriptions: Test functions
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char wo[] = "_putchar\n";
+	int i;
+	for (i = 0 ; wo[i] != '\0'; i++)
+	{
+		_putchar(wo[i]);
+	}
+	return (0);
+}
