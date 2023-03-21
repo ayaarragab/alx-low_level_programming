f469fc8580a85d14ac965c16ff2a3f0789aae632
Aya Ragab <git@github.com>
2023-03-21 13:13:21 -0700

    sa

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line.
+ * Return : nothing
+ */
+void print_alphabet(void)
+{
+	char new_line = '\n';
+	char i;
+
+	for(i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar(new_line);
+	return;
+}
