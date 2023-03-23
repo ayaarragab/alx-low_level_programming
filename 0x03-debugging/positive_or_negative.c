603f1b2a4eb717086b7d337ad4c440b59a6dc538
Aya Ragab <git@github.com>
2023-03-23 01:23:01 -0700

    kk

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+*positive_or_negative - entry point of programs
+*
+*Return: nothing
+*/
+void positive_or_negative(int n)
+}
+	if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else
+		printf("%d is zero\n", n);
+}
