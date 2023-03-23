116d21b50f3688c9f1188f5d4cb4f9c1cd9d105d
Aya Ragab <git@github.com>
2023-03-23 05:01:49 -0700

    ewewe

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,18 +1,14 @@
-#include <ctype.h>
-#include <stdio.h>
+#include "main.h"
 /**
-* isdigit - is it a digit
-* Return: 1 if digit and 0 if not
-* @c: variable test
+*_isdigit - function that verifies if a  character is a digit or not
+*@c: tested character
+*Return: returns 1 if it is uppercase , 0 if not
 */
+
 int _isdigit(int c)
 {
-	if (isdigit(c))
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+if ((c >= 48) && (c <= 57))
+return (1);
+
+return (0);
 }
