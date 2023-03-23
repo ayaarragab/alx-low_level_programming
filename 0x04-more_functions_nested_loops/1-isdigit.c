1b1e07cc0ce55c43de66606a8337bd768c2d05a6
Aya Ragab <git@github.com>
2023-03-23 04:55:15 -0700

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include <ctype.h>
+/**
+* isdigit - is it a digit
+* Return: 1 if digit and 0 if not
+* @c: variable test
+*/
+int _isdigit(int c)
+{
+	if (isdigit(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
