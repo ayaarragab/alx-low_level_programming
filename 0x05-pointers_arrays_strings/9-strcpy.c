d1883444db87e694ca88b1db71b545635331e59a
Aya Ragab <git@github.com>
2023-03-27 12:40:36 -0700

    pp

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcpy - copy string src to dest
+ * Return: dest pointer
+ * @dest: variable1 test
+ * @src: variable2 test
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i, len;
+
+	len = strlen(src);
+
+	for (i = 0; i < len ; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
