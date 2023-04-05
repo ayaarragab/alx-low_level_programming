6120165406b9ade20a2b7edd955fce3a7830efb8
Aya Ragab <git@github.com>
2023-04-05 11:27:35 -0700

    my own power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _pow_recursion - calculates power
+ * @x: base of expo
+ * @y: expo
+ * Return: power
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+	{
+		return (1);
+	}
+	else if (y < 0)
+	{
+		return (-1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
