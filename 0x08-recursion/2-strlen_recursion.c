92eeebec8a51dd1e84383afd9fbf3b17e636e5a9
Aya Ragab <git@github.com>
2023-04-05 09:13:39 -0700

    my own strlen

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _str_recursion - print kength of string recursively
+ * @s: string test
+ * Return: lendthgof the string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*(s) == '\0')
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(s + 1));
+}
