8eb67f796c5d7b3cf2450e47dc863c0da802ab3e
Aya Ragab <git@github.com>
2023-04-09 14:12:07 -0700

    2

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point for func
+ * @args: array length
+ * @argv: array
+ * Return: 0
+ */
+int main(int args, char *argv[] __attribute__((unused)))
+{
+printf("%d\n", args);
+return (0);
+}
