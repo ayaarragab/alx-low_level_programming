53eef6fcc5ab47756f367963c2b0547cf46e3534
Aya Ragab <git@github.com>
2023-04-10 08:47:32 -0700

    44

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,31 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+/**
+ * check_if_str_is_digit - check if string is digit
+ * @str: string pointer
+ * Return: 0
+ */
+int check_if_str_is_digit(char *str)
+{
+	unsigned int long counter;
+
+	for (counter  = 0; counter < strlen(str) ; counter++)
+	{
+		if (!isdigit(str[counter]))
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
+
 /**
  * main - entry point unc
  * @argc: length of vector array
  * @argv: vector array
  * Return: 0 if success 1 if fail
  */
+
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+	int i;
 
-	for (i = 1; i < argc ; i++)
+	int str_to_dig;
+
+	int sum = 0;
+
+	i = 1;
+	while (i < argc)
 	{
-		if (argv[i] == NULL)
+		if (check_if_str_is_digit(argv[i]))
 		{
-			printf("%d\n", 0);
+			str_to_dig = atoi(argv[i]);
+			sum = sum + str_to_dig;
 		}
-		if (!atoi(argv[i]))
+		else
 		{
 			printf("Error\n");
 			return (1);
 		}
-		if (atoi(argv[i]) > 0)
-		{
-		sum += atoi(argv[i]);
-		}
+		i++;
 	}
 	printf("%d\n", sum);
+
 	return (0);
 }
