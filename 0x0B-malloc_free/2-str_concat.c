2ffe659fec7d5ae1565284f9133646c4d30e7e7e
Aya Ragab <git@github.com>
2023-04-13 17:41:04 -0700

    df

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -20,9 +20,9 @@ char *str_concat(char *s1, char *s2)
 	int j;
 
 	if (s2 == NULL && s1 != NULL)
-		return (s1);
+		s2 = "";
 	if (s1 == NULL && s2 != NULL)
-		return (s2);
+		s1 = "";
 	if (s1 == NULL && s2 == NULL)
 		return (NULL);
 
@@ -34,7 +34,7 @@ char *str_concat(char *s1, char *s2)
 		con[i] = s1[i];
 
 	j = 0;
-	for (i = size1; i < size1 + size2; i++)
+	for (i = size1; i < size2; i++)
 	{
 		con[i] = s2[j];
 		j++;
