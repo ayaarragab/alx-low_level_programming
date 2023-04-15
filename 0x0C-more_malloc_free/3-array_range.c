386ebc10d99220d7999f2e5bcf1d098984415305
ayaragab <you@example.com>
2023-04-15 06:05:42 +0200

    yarab

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -22,7 +22,7 @@ int *array_range(int min, int max)
     {
         return (NULL);
     }
-    for ( i = 0; i <= ((max - min) + 1); i++)
+    for ( i = 0; i <= (max - min); i++)
     {
         ptr[i] = i + min;
     }
