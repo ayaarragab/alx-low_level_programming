69dfa443072cf5ecd6f3c98c8c0f248238c69fe1
ayaragab <you@example.com>
2023-04-15 04:36:04 +0200

    mod

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -34,7 +34,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		n = len2;
 	}
-	len3 = len1 + n + 1;
+	len3 = len1 + n;
 	result = malloc(len3 * sizeof(char));
 	if (result == NULL)
 	{
