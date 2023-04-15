25250e8cafcb1230bb0fb34f426df9b2216738ca
root <root@DESKTOP-IJODIBG.localdomain>
2023-04-15 02:36:50 +0200

    first one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include <main.h>
+#include <stdlib.h>
+/**
+ * malloc_checked - malloc with handelling
+ * @b: size
+ * Return: nothing
+*/
+void *malloc_checked(unsigned int b)
+{
+    int *ptr;
+
+    ptr = malloc(sizeof(int) * b);
+    if (ptr == NULL)
+    {
+        exit(98);
+    }
+    return (ptr);
+    
+}
