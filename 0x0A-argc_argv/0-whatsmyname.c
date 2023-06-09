8b1ade5bce533491e56b62621316c7cb0af07fd1
Aya Ragab <git@github.com>
2023-06-09 12:24:22 +0300

    I hate bettty

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+/**
+ * main - main function
+ * @argc: vector array length
+ * @argv: vector array
+ * Return: 0
+*/
 int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s", argv[0]);
 	printf("\n");
-	return(0);
+	return (0);
 }
