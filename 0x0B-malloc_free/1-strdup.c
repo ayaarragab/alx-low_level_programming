215a20f8ca1b1e3c8f35851435288589f51d3e2b
Aya Ragab <git@github.com>
2023-06-13 15:06:11 +0300

    Orange

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _strdup - duplicates a string
+ * @str: string variable
+ * Return: string pointer
+*/
+char *_strdup(char *str)
+{
+	char *new_string;
+	unsigned int size, i;
+
+	size = strlen(str) + 1;
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	new_string = (char *)malloc(size);
+	if (new_string == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		new_string[i] = str[i];
+	}
+	new_string[size] = '\0';
+	return (new_string);
+}
