83dc1977b04af4d1e955c270fdb717f7a041763d
Aya Ragab <git@github.com>
2023-06-13 14:35:02 +0300

    Your lie in April

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - creats an array
+ * @size: size var
+ * @c: char
+ * Return: character pointer
+*/
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+
+	char *array_chars;
+
+	array_chars = (char *)malloc(size);
+
+	if (size == 0 || c == '\0')
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		*(array_chars + i) = c;
+	}
+	array_chars[size] = '\0';
+	return (array_chars);
+	free(array_chars);
+}
