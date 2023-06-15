51229b0e92de315ac6be295e9fabe4f9072b1d00
Aya Ragab <git@github.com>
2023-06-15 19:24:41 +0300

    4th

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,13 +11,15 @@ int *array_range(int min, int max)
 {
 	int *array;
 
-	int numbers_length, i, k, n;
-
+	int numbers_length, k, n;
+	if ((unsigned int)(max - min) >= (unsigned int)((__SIZE_MAX__)/sizeof(int)))
+	{
+		return (NULL);
+	}
+	
 	if (min > max)
 		return (NULL);
-	numbers_length = 1;
-	for (i = min; i <= max; i++)
-		numbers_length++;
+	numbers_length = max - min + 1;
 	array = (int *)malloc(numbers_length * sizeof(int));
 	if (array == NULL)
 	{
