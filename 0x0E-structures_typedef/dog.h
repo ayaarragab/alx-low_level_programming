4ec349a8a5ef0424d2f8ae1be50fe0f2c1e40b08
Aya Ragab <git@github.com>
2023-06-19 15:15:06 +0300

    struct and typef4

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,18 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
-
 /**
- * struct dog - dog structure
+ * typedef struct dog - dog structure
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
 */
-struct dog
-{
+typedef struct dog{
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
