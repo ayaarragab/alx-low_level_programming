65f68817d417a52d7ec5c2ae8e6ebb8abf7ba0c8
Aya Ragab <git@github.com>
2023-06-19 15:07:26 +0300

    3rd

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,13 +11,13 @@ void print_dog(struct dog *d)
 if (d != NULL)
 {
 	if (d->name == NULL)
-		printf("Name: (nil)");
+		printf("Name: (nil)\n");
 	else
-		printf("Name: %s", d->name);
+		printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
 	if (d->owner == NULL)
-		printf("owner: (nil)");
+		printf("owner: (nil)\n");
 	else
-		printf("owner: %s", d->name);
-	printf("Age: %f", d->age);
+		printf("owner: %s\n", d->owner);
 }
 }
