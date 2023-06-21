162d6635305101b79d4515bebbd8467323c0f146
Aya Ragab <git@github.com>
2023-06-21 14:59:40 +0300

    Updated

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 /**
  * print_name - func
  * @name: string to be printed
@@ -7,5 +8,9 @@
 */
 void print_name(char *name, void (*f)(char *))
 {
+    if (name == NULL)
+    {
+        return;
+    }
     f(name);
 }
