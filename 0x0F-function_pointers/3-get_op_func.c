d4b048e6b01ccd349eb430fedef82b7277cd98a8
Aya Ragab <git@github.com>
2023-06-22 01:43:57 +0300

    for loop is not allowed

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -17,10 +17,11 @@ int (*get_op_func(char *s))(int, int)
 
 	int i;
 
-	for (i = 0; i < 5; i++)
+	while (i < 5)
 	{
 		if (strcmp(ops[i].op, s) == 0)
 			return (ops[i].f);
+		i++;
 	}
 	return (NULL);
 }
