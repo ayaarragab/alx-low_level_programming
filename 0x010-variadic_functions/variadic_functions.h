fe8eb2f05ea312649d68a7da2761853e6c5160db
Aya Ragab <git@github.com>
2023-06-22 11:51:58 +0300

    1. To be is to be the value of a variable

--- 0x010-variadic_functions/variadic_functions.h
+++ 0x010-variadic_functions/variadic_functions.h
@@ -2,6 +2,10 @@
 #define VARIADIC_FUNCTIONS_H
 #include <stdarg.h>
 #include <stdlib.h>
+#include <stdio.h>
+
 int sum_them_all(const unsigned int n, ...);
 int _putchar(char c);
+void print_numbers(const char *separator, const unsigned int n, ...);
+
 #endif
