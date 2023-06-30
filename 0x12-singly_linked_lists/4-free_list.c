62d82a72cbcfc46130629fe5bc566ac9b2e144ea
ayaarragab <ayarsk4343@gmail.com>
2023-07-01 02:38:14 +0300

    uppu

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -6,13 +6,13 @@
 */
 void free_list(list_t *head)
 {
-    list_t *current = head, *next_node;
+	list_t *current = head, *next_node;
 
-    while (current != NULL)
-    {
-        next_node = current->next;
-        free(current->str);
-        free(current);
-        current = next_node;
-    }
+	while (current != NULL)
+	{
+		next_node = current->next;
+		free(current->str);
+		free(current);
+		current = next_node;
+	}
 }
