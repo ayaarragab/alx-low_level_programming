38ee7c5a80b64f7d1d89d8fe79195cc5901974f8
ayaarragab <ayarsk4343@gmail.com>
2023-07-05 05:17:13 +0300

    6666

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,6 +8,8 @@ void free_listint2(listint_t **head)
 {
 	listint_t *node = *head, *stored;
 
+	if (head == NULL || *head == NULL)
+		return;
 	if (node == NULL)
 		return;
 	while (node != NULL)
---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * pop_listint - del
+ * @head: pointer to pointer
+ * Return: n
+*/
+int pop_listint(listint_t **head)
+{
+
+}
