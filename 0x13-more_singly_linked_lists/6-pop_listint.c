5e2d08e0be1601d23b5f969c1f8a347b7b6d2cde
ayaarragab <ayarsk4343@gmail.com>
2023-07-05 05:33:57 +0300

    07

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -10,7 +10,7 @@ int pop_listint(listint_t **head)
 
 	int value;
 
-	if (del == NULL || del->next == NULL || *head == NULL || head == NULL)
+	if (*head == NULL || head == NULL)
 		return (0);
 	*head = del->next;
 	value = del->next->n;
