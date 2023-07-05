9aba53c720b7dcb81c5e7c1ef9f209eb2e4ea14d
ayaarragab <ayarsk4343@gmail.com>
2023-07-05 05:05:42 +0300

    6

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_listint2 - free list
+ * @head: list's head
+ * Return: nothing
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *node = malloc(sizeof(listint_t)), *stored;
+
+	node = *head;
+	while (node != NULL)
+	{
+		stored = node->next;
+		free(node);
+		node = stored;
+	}
+	*head = NULL;
+}
