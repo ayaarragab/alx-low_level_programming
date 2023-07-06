bcb6739e37f4a381c97187bf7e8963ff0bc6a572
ayaarragab <ayarsk4343@gmail.com>
2023-07-06 18:10:53 +0300

    1

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * power - power
+ * @x: num
+ * @y: num
+ * Return: num
+*/
+unsigned int power(unsigned int x, unsigned int y)
+{
+	if (y == 0)
+		return (1);
+	return (x * (power(x, y - 1)));
+}
+/**
+ * binary_to_uint - bin to uint
+ * @b: bin str
+ * Return: num or 0
+*/
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int length = strlen(b), i = 0, cntr = length - 1, num = 0;
+
+	if (b == NULL)
+		return (0);
+	while (b[i] != '\0')
+	{
+		if (b[i] == '1')
+		{
+			num += power(2, cntr);
+			cntr--;
+			i++;
+		}
+		else if (b[i] == '0')
+		{
+			cntr--;
+			i++;
+			continue;
+		}
+		else
+			return (0);
+	}
+	return (num);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+unsigned int binary_to_uint(const char *b);
+#endif
