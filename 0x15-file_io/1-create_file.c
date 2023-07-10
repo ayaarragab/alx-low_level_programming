0f50b702ab02cc4086ff6fefb46901900953df3f
ayaarragab <ayarsk4343@gmail.com>
2023-07-10 20:05:58 +0300

    2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 		close(fd);
 		return (1);
 	}
-	tw = write(fd, filename, strlen(text_content));
+	tw = write(fd, text_content, strlen(text_content));
 	if (tw == -1)
 	{
 		close(fd);
