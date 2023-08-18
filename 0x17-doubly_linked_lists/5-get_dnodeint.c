d7229f15fa360aec69bddf0b6e5cb5f8668ef15b
ayaarragab <ayarsk4343@gmail.com>
2023-08-18 19:17:59 +0300

    getget

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,5 +1,22 @@
 #include "lists.h"
 /**
+ * calc_len_list - calculates length of a list
+ * @head: head
+ * Return: length
+*/
+int calc_len_list(dlistint_t *head)
+{
+	dlistint_t *temp = head;
+	size_t count = 0;
+
+	while (temp != NULL)
+	{
+		count++;
+		temp = temp->next;
+	}
+	return (count);
+}
+/**
  * get_dnodeint_at_index - get node at specific index
  * @head: head
  * @index: index
@@ -10,7 +27,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	dlistint_t *temp = head;
 	unsigned int count = 0;
 
-	if (head == NULL)
+	if (head == NULL || index >= calc_len_list(temp))
 		return (NULL);
 	while (count < index && temp->next != NULL)
 	{
