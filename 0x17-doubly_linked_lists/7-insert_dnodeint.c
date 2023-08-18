eeb234c9c2c23c3a388993ccfdf75a55da6e7f7f
ayaarragab <ayarsk4343@gmail.com>
2023-08-18 20:58:41 +0300

    7-insert_2nodes_يارب

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -53,6 +53,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		nextnode->prev = new_node;
 		return (new_node);
 	}
+	temp->next = new_node;
 	new_node->prev = temp;
 	new_node->next = NULL;
 	return (new_node);
