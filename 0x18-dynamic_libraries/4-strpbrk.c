f80e737320970280506790a70045716664214cac
ayaarragab <ayarsk4343@gmail.com>
2023-08-24 16:52:14 +0200

    dynamic lib

---
+++ 0x18-dynamic_libraries/4-strpbrk.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _strpbrk - func
+ * @s: string
+ * @accept: string
+ * Return: pointer on s
+*/
+char *_strpbrk(char *s, char *accept)
+{
+}
---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * _abs - func
+ * @n: num
+ * Return: positive n
+*/
+int _abs(int n)
+{
+}
