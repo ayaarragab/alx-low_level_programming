fad67118835c3961489f04aca524f08f94edfb02
ayaarragab <ayarsk4343@gmail.com>
2023-09-08 17:55:35 +0300

    0-creat

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,21 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of the hash tables (maximum number of elements)
+ * Return: pointer on the hash table or null in failures
+*/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table = malloc(sizeof(hash_table_t));
+
+	if (table == NULL)
+		return (NULL);
+	table->size = size;
+	table->array = calloc(size, sizeof(hash_node_t *));
+	if (table->array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
+	return (table);
+}
---
+++ 0x1A-hash_tables/0-main.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+
+/**
+ * main - check the code for
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+    hash_table_t *ht;
+
+    ht = hash_table_create(1024);
+    printf("%p\n", (void *)ht);
+    free(ht->array);
+    free(ht);
+    return (EXIT_SUCCESS);
+}
