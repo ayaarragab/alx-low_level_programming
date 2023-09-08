80b415a903a9244f2bb28ee77d002823c9db9b5f
ayaarragab <ayarsk4343@gmail.com>
2023-09-08 17:56:24 +0300

    0-creat

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,7 +11,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (table == NULL)
 		return (NULL);
 	table->size = size;
-	table->array = calloc(size, sizeof(hash_node_t *));
+	table->array = malloc(size * sizeof(hash_node_t *));
 	if (table->array == NULL)
 	{
 		free(table);
