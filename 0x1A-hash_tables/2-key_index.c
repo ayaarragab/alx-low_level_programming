3256730843b24329611e28c34a77d123154130ea
ayaarragab <ayarsk4343@gmail.com>
2023-09-08 18:24:38 +0300

    You're allowed to cheat00

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -7,7 +7,7 @@
 */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    unsigned long int index = hash_djb2(key);
+	unsigned long int index = hash_djb2(key);
 
-    return (index % size);
+	return (index % size);
 }
