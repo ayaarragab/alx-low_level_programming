9603dd148c9372e151568dac87c32bbc55185251
ayaarragab <ayarsk4343@gmail.com>
2023-09-09 14:41:10 +0300

    3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,7 +13,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht == NULL || key == NULL)
 		return (0);
-	idx = key_index(key, ht->size);
+	idx = key_index((const unsigned char *)key, ht->size);
 	tbArrNode = ht->array[idx];
 	while (tbArrNode)
 	{
