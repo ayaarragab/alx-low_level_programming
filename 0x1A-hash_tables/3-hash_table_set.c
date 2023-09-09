51205b448a55158bfc08e261ca29cd6fb466491a
ayaarragab <ayarsk4343@gmail.com>
2023-09-09 14:39:01 +0300

    3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,50 +1,5 @@
 #include "hash_tables.h"
 /**
- * create_item - creates a node
- * @key: key
- * @value: value
- * Return: key
- */
-hash_node_t *create_item(char *key, char *value)
-{
-	hash_node_t *item = malloc(sizeof(hash_node_t));
-
-	if (item == NULL)
-		return (NULL);
-	item->key = malloc(sizeof(key));
-	if (item->key == NULL)
-		return (NULL);
-	item->value = malloc(sizeof(value));
-	if (item->value == NULL)
-		return (NULL);
-	strcpy(item->key, key);
-	strcpy(item->value, value);
-	return (item);
-}
-/**
- * free_hash_table - frees a hash table
- * @table: table
- * @index: index
- * Return: nothing
-*/
-void free_hash_table(hash_table_t **table, unsigned long int index)
-{
-	hash_node_t *temp;
-	unsigned long int i;
-
-	for (i = index; (*table)->array[i]; i++)
-	{
-		temp = (*table)->array[i];
-		(*table)->array[i] = (*table)->array[i]->next;
-		free(temp->key);
-		free(temp->value);
-		free(temp);
-	}
-	free((*table)->array);
-	free(*table);
-}
-
-/**
  * hash_table_set - set value to specifc key
  * @ht: hash table
  * @key: key
@@ -53,40 +8,42 @@ void free_hash_table(hash_table_t **table, unsigned long int index)
 */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index = key_index((const unsigned char *)key, ht->size);
-	hash_node_t *item;
+	unsigned long int idx;
+	hash_node_t *tbArrNode, *new_item;
 
-	if (ht == NULL || key == NULL || index > ht->size)
+	if (ht == NULL || key == NULL)
 		return (0);
-	if (value == NULL)
-		strcpy((char *)value, "");
-	if ((ht->array)[index] == NULL)
+	idx = key_index(key, ht->size);
+	tbArrNode = ht->array[idx];
+	while (tbArrNode)
 	{
-		hash_node_t *item = create_item((char *)key, (char *)value);
-
-		item->next = NULL;
-		(ht->array)[index] = item;
-		free_hash_table(&ht, index);
-		return (1);
+		if (strcmp(tbArrNode->key, key) == 0)
+		{
+			free(tbArrNode->value);
+			tbArrNode->value = strdup(value);
+			if (tbArrNode->value == NULL)
+				return (0);
+			return (1);
+		}
+		tbArrNode = tbArrNode->next;
 	}
-	item = malloc(sizeof(hash_node_t));
-	if (item == NULL)
+	new_item = malloc(sizeof(hash_node_t));
+	if (new_item == NULL)
+		return (0);
+	new_item->key = strdup(key);
+	if (new_item->key == NULL)
 	{
-		free_hash_table(&ht, index);
+		free(new_item);
 		return (0);
 	}
-	item = create_item((char *)key, (char *)value);
-	if ((ht->array)[index]->next == NULL)
+	new_item->value = strdup(value);
+	if (new_item->value == NULL)
 	{
-		(ht->array)[index]->next = item;
-		item->next = NULL;
-		free_hash_table(&ht, index);
-		return (1);
+		free(new_item->key);
+		free(new_item);
+		return (0);
 	}
-	while ((ht->array)[index]->next)
-		(ht->array)[index] = (ht->array)[index]->next;
-	(ht->array)[index]->next = item;
-	item->next = NULL;
-	free_hash_table(&ht, index);
+	new_item->next = ht->array[idx];
+	ht->array[idx] = new_item;
 	return (1);
 }
