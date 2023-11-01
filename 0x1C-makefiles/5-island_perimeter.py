3eddf34596d28f3fb12a2eff70618276d8dffd06
ayaarragab <ayarsk4343@gmail.com>
2023-11-01 15:09:33 +0200

    5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -7,19 +7,22 @@ in a grid
 
 
 def island_perimeter(grid):
+    """
+    :grid: grid to calculate its premiter
+    :returns the perimeter of the island described
+    """
     perimeter = 0
     rows = len(grid)
     cols = len(grid[0])
-    
+
     for i in range(rows):
         for j in range(cols):
             if grid[i][j] == 1:
                 perimeter += 4
-                
+
                 if i > 0 and grid[i - 1][j] == 1:
                     perimeter -= 2
                 if j > 0 and grid[i][j - 1] == 1:
                     perimeter -= 2
-    
-    return perimeter
 
+    return perimeter
