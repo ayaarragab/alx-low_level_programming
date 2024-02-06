5c1417f35a9bc22bc461154e81e608c2f52ffe0c
ayaarragab <ayarsk4343@gmail.com>
2024-02-06 15:20:03 +0200

    finish ISA

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,34 @@
+#include "search_algos.h"
+/**
+ * binary_search - searches array using the Binary search algorithm
+ * @array: array of integers
+ * @size: size
+ * @value: value to be searched for
+ * Return: -1 if faliure and the index if success
+*/
+int binary_search(int *array, size_t size, int value)
+{
+	size_t r = size - 1, l = 0, mid, i;
+
+	while (l <= r)
+	{
+		printf("Searching in array: ");
+		for (i = l; i <= r; i++)
+		{
+			if (i == r)
+			{
+				printf("%d\n", array[i]);
+				break;
+			}
+			printf("%d, ", array[i]);
+		}
+		mid = (r + l) / 2;
+		if (array[mid] == value)
+			return (mid);
+		else if (value < array[mid])
+			r = mid - 1;
+		else
+			l = mid + 1;
+	}
+	return (-1);
+}
